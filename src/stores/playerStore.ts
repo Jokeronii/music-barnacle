4bc5a0eba50961f8703251a28e7e56cbd21b2497
Jokeronii <tendavid2@gmail.com>
2025-07-19 11:32:31 +0700

    feat(store):create player store

--- src/stores/playerStore.ts
+++ src/stores/playerStore.ts
@@ -1,20 +1,49 @@
 import { create } from 'zustand';
 
-type Todo = {
-  id: number;
+type Track = {
+  id: string;
   title: string;
-  completed: boolean;
+  artist: string;
+  src: string;
+  cover: string;
 };
-interface TodoState {
-  todos: Todo[];
-  addTodo: (title: string) => void;
-  deleteTodo: (id: number) => void;
-  toggleTodo: (id: number) => void;
-}
+type playerState = {
+  //properties
+  playList: Track[];
+  currentIndex: number;
+  isPlaying: boolean;
+  volume: number;
 
-export const useTodoStore = create<TodoState>((set) => ({
-  todos: [],
-  addTodo: (title) => set((state) => ({ todos: [...state.todos, { id: Date.now(), title, completed: false }] })),
-  deleteTodo: (id) => set((state) => ({ todos: state.todos.filter((todos) => todos.id !== id) })),
-  toggleTodo: (id) => set((state) => ({ todos: state.todos.map((todos) => (todos.id === id ? { ...todos, completed: !todos.completed } : todos)) })),
+  //methods
+  setPlayList: (playList: Track[]) => void;
+
+  play: (index: number) => void;
+  pause: () => void;
+  resume: () => void;
+  next: () => void;
+  prev: () => void;
+  setVolume: (v: number) => void;
+};
+
+export const usePlayerStore = create<playerState>((set, get) => ({
+  playList: [],
+  currentIndex: 0,
+  isPlaying: false,
+  volume: 100,
+
+  setPlayList(playList) {},
+  play() {},
+  pause() {
+    set({ isPlaying: false });
+  },
+  resume() {
+    set({ isPlaying: true });
+  },
+  next() {},
+
+  prev() {},
+
+  setVolume(v): number {
+    return v;
+  },
 }));
